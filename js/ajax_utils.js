c7e60309bf3d46c7432fb6bf200375bc27ae2296
Damien <damien.sornette@gmail.com>
2018-01-14 18:20:12 +0100

    * Bug : Correction de l'appel à l'api des animations pour correspondre au taf de Cyril

--- js/ajax_utils.js
+++ js/ajax_utils.js
@@ -3,11 +3,8 @@ const AJAX_FAILURE = 2;
 const AJAX_ERROR = 3;
 //Configuration des lancements d'animations
 // /!\Attention là du est dans du js pas dans du php ;)/!\
-//En gros pour lancer une anim on appelera $ANIM_BASE_URL 
-//avec un argument en POST, dans la variable "code" (cf. ajax_utils.js)
-//Tu peux tout changer la dedans pour que ca marche avec ton serveur, c'est même
-//ok si tu pousses sur le git. Tant que tu gardes le système base + constante
-const ANIM_BASE_URL = 'http://127.0.0.1:8080/animations.py';
+//En gros pour lancer une anim on appelera $ANIM_BASE_URL + une constante de code
+const ANIM_BASE_URL = 'http://127.0.0.1:8080/';
 //constantes de code
 const ANIM_CODE_CHECK_UP = "checkup";
 const ANIM_CODE_DIAGNOSTIC_DROGUE = "diagnostic_drogue";
@@ -26,7 +23,6 @@ const ANIM_CODE_TEST_GROSSESSE = "test_grossesse";
 //La méthode de lancement de l'animation
 function play_anim(code_anim) {
     $(document).ready(function(){
-        var input = {code: code_anim};
-        $.post(ANIM_BASE_URL,input, function(){});
+        $.post(ANIM_BASE_URL+code_anim);
     });
 }
