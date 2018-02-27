34a2a0de1d753adcb4a4608d5a34dcdc1f37e61e
Damien <damien.sornette@gmail.com>
2018-02-27 22:50:08 +0100

    * Css :'(

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
+
 // Configuration de la  base de données
 $DB_STRING = "mysql:host=localhost;dbname=tlo;charset=utf8";
 $DB_LOGIN = "root";
@@ -8,6 +9,7 @@ $DB_PASSWORD = "";
 $STANDARD_MENU = 'STANDARD';
 $ADN_MENU = 'ADN';
 
+//Ne pas mettre plus de 4 users. Ni moins. Penser à mettre dans /img/user_portraits les portraits des joueurs, nommés user1.jpg, user2.jpg, ...
 $USERS = array('user1' => 'password1', 'user2' => 'password2', 'user3' => 'password3', 'user4' => 'password4');
 $USER_MENU = array('user1' => $STANDARD_MENU, 'user2' => $ADN_MENU, 'user3' => $STANDARD_MENU, 'user4' => $STANDARD_MENU);
 ?>
--- views/home.php
+++ views/home.php
@@ -1,36 +1,47 @@
 <?php require_once 'header.php'; ?>
+<svg class="clip-svg">
+<defs>
+<clipPath id="hexagon-clip" clipPathUnits="objectBoundingBox">
+    <polygon points="0.25 0.05, 0.75 0.05, 1 0.5, 0.75 0.95, 0.25 0.95, 0 0.5"></polygon>
+</clipPath>
+</defs>
+</svg>
 <script>
     var userClickHandler = function (id) {
         window.location.href = './login.php?id=' + id;
     };
     var documentReadyHandler = function () {
         $('#user1').click(function () {
-            userClickHandler('user1');
+            userClickHandler('<?php echo array_keys($USERS)[0]; ?>');
         });
         $('#user2').click(function () {
-            userClickHandler('user2');
+            userClickHandler('<?php echo array_keys($USERS)[1]; ?>');
         });
         $('#user3').click(function () {
-            userClickHandler('user3');
+            userClickHandler('<?php echo array_keys($USERS)[2]; ?>');
         });
         $('#user4').click(function () {
-            userClickHandler('user4');
+            userClickHandler('<?php echo array_keys($USERS)[3]; ?>');
         });
     };
     $(document).ready(documentReadyHandler);
 </script>
 <div class='home_container'>
     <div class='home_user' id='user1'>
-        <img src="/img/user_1.png" alt="user1"/>
+        <img class="home_user_back" src="/img/user_1.png" alt="user1"/>
+        <img class="home_user_portrait" src="/img/user_portraits/user1.jpg"  alt="user1"/>
     </div>
     <div class='home_user' id='user2'>
-        <img src="/img/user_2.png" alt="user2"/>
+        <img class="home_user_back" src="/img/user_2.png" alt="user2"/>
+        <!--<img class="home_user_portrait"  src="/img/user_portraits/user2.jpg" alt="user2"/>-->
     </div>
     <div class='home_user' id='user3'>
-        <img src="/img/user_1.png" alt="user3"/>
+        <img class="home_user_back" src="/img/user_1.png" alt="user3"/>
+        <!--<img class="home_user_portrait"  src="/img/user_portraits/user3.jpg" alt="user3"/>-->
     </div>
     <div class='home_user' id='user4'>
-        <img src="/img/user_2.png" alt="user4"/>
+        <img class="home_user_back" src="/img/user_2.png" alt="user4"/>
+        <!--<img class="home_user_portrait"  src="/img/user_portraits/user4.jpg" alt="user4"/>-->
     </div>
 </div>
 <?php require_once 'footer.php'; ?>
