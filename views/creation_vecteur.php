e8e4e35fbc66475b1d8c8496c7c472cda58f1078
Damien <damien.sornette@gmail.com>
2018-03-20 23:33:10 +0100

    * Dev + CSS

--- views/creation_vecteur.php
+++ views/creation_vecteur.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once 'header.php';
 require_once '../controllers/check_login.php';
 require_once '../db.php';
@@ -16,7 +15,9 @@ try {
 }
 $en_creation = ($count > 0);
 
-function generate_list_patients_content() {
+//Génération de la liste
+function generate_list_patients_content($db) {
+    $liste_patients = "";
     try {
         $query = $db->prepare("select p.rfid, p.nom "
                 . "from patients p "
@@ -32,6 +33,7 @@ function generate_list_patients_content() {
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
+    return $liste_patients;
 }
 ?>
 <script>
@@ -40,10 +42,13 @@ function generate_list_patients_content() {
         if (id !== "-1") {
             var input = {rfid: id};
             $.post('/ajax/ajax_get_adn_patient.php', input, getADNHandler);
+        }else{
+            $('#creation_vecteur_saisie *').hide();
         }
     };
 
     var getADNHandler = function (output) {
+        alert(output);
         var json = JSON.parse(output);
         switch (json.status) {
             case AJAX_SUCCESS :
@@ -52,15 +57,15 @@ function generate_list_patients_content() {
                 $('#adn_3').text(json.base3);
                 $('#adn_4').text(json.base4);
                 $('#error').hide();
-                $('#zone_saisie').show();
+                $('#creation_vecteur_saisie *').show();
                 break;
             case AJAX_FAILURE :
                 $('#error').text(json.message);
                 $('#error').show();
-                $('#zone_saisie').hide();
+                $('#creation_vecteur_saisie *').hide();
                 break;
             case AJAX_ERROR :
-                $('#zone_saisie').hide();
+                $('#creation_vecteur_saisie *').hide();
                 alert(json.message);
                 break;
         }
@@ -73,18 +78,21 @@ function generate_list_patients_content() {
                 window.location.href = 'views/creation_vecteur.php';
                 break;
             case AJAX_FAILURE :
-                //FIXME : gestion correcte de l'erreur
                 alert(json.message);
                 break;
             case AJAX_ERROR :
-                $('#zone_saisie').hide();
+                $('#creation_vecteur_saisie *').hide();
                 alert(json.message);
                 break;
         }
     }
 
-    var vecteurKeyUpHandler = function () {
-        //FIXME : interdire les non A T G C
+    var vecteurKeyUpHandler = function (event) {
+        var val = String.fromCharCode(event.which).toUpperCase();
+        if (val === 'A' || val === 'T' || val === 'G' || val === 'C') {
+            $(this).val(val);
+        }
+        event.preventDefault();
     };
 
     var creerVecteurHandler = function () {
@@ -100,31 +108,43 @@ function generate_list_patients_content() {
 
     var documentReadyHandler = function () {
         $('#liste_patients').change(listeChangedHandler);
-        $('#zone_saisie').hide();
         $('#btn_vecteur').click(creerVecteurHandler);
+        $("#vecteur_1").keypress(vecteurKeyUpHandler);
+        $("#vecteur_2").keypress(vecteurKeyUpHandler);
+        $("#vecteur_3").keypress(vecteurKeyUpHandler);
+        $("#vecteur_4").keypress(vecteurKeyUpHandler);
     };
     $(document).ready(documentReadyHandler);
 </script>
-<br/>
-<span id="error"></span>
-<br/>
-<div id ="zone_saisie">
-    <div id="adn">
-        <span>ADN</span>
-        <span id = 'adn_1'></span>
-        <span id = 'adn_2'></span>
-        <span id = 'adn_3'></span>
-        <span id = 'adn_4'></span>
+<div class = "standard_page_body">
+    <div class="standard_page_header"></div>
+    <div class="standard_page_content">
+        <div class="creation_vecteur_content">
+            <div class="creation_vecteur_list">
+                <select id="liste_patients">
+                    <?php echo generate_list_patients_content($db); ?>
+                </select>
+            </div>
+            <div id ="creation_vecteur_saisie" class="creation_vecteur_saisie">
+                <div id="adn" class="creation_vecteur_show_adn" style='display:none'>
+                    <span id = 'adn_1'></span>
+                    <span id = 'adn_2'></span>
+                    <span id = 'adn_3'></span>
+                    <span id = 'adn_4'></span>
+                </div>
+                <div id="vecteur" class="creation_vecteur_saisie_adn" style='display:none'>
+                    <input type="text" id = 'vecteur_1' maxlength='1'/>
+                    <input type="text" id = 'vecteur_2' maxlength='1'/>
+                    <input type="text" id = 'vecteur_3' maxlength='1'/>
+                    <input type="text" id = 'vecteur_4' maxlength='1'/>
+                </div>
+                <div class="creation_vecteur_creer">
+                    <img src="/img/bouton_enter.png" alt="Créer" id="btn_vecteur" style='display:none'/>
+                </div>
+            </div>
+        </div>
     </div>
-    <div id="vecteur">
-        <span>Vecteur</span>
-        <input type="text" id = 'vecteur_1' maxlength='1'/>
-        <input type="text" id = 'vecteur_2' maxlength='1'/>
-        <input type="text" id = 'vecteur_3' maxlength='1'/>
-        <input type="text" id = 'vecteur_4' maxlength='1'/>
-    </div>
-    <input type="submit" value="Créer le vecteur" id="btn_vecteur"/>
+    <?php require_once './fragments/bouton_retour.php' ?>
 </div>
 <?php
-
 require_once 'footer.php';
